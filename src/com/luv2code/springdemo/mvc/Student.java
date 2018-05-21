302f1d10b6f3de6c8d7b57cb5d0f189fa09214c4
John Furtado <jf8846@gmail.com>
2018-05-21 19:27:01 -0400

    drop-down lists

--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -1,13 +1,27 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	
 	private String firstName;
 	private String lastName;
 	
 	private String country;
+	
+	private LinkedHashMap<String, String> countryOptions;
 
-	public Student() {	
+	public Student() {
+		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>(); 
+	
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India ");
+		countryOptions.put("US", "United States of America");
+
 	}
 
 	public String getFirstName() {
@@ -32,6 +46,10 @@ public class Student {
 
 	public void setCountry(String country) {
 		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 	
 	
